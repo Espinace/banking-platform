34c484e7df391a768e0b8d684aeac31e19ca4940
Bruno <brunoaespinace@hotmail.com>
2026-05-26 11:41:37 -0300

    Non hardcoded secret key and expiration, also using UserDetails now on isTokenValid for JwtService

--- src/main/java/com/bruno/banking_platform/security/JwtService.java
+++ src/main/java/com/bruno/banking_platform/security/JwtService.java
@@ -2,6 +2,8 @@ package com.bruno.banking_platform.security;
 
 import com.bruno.banking_platform.auth.domain.User;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -13,35 +15,28 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY =
-            "YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXoxMjM0NTY=";
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
 
     private Key getSignInKey() {
-
-        byte[] keyBytes =
-                Decoders.BASE64.decode(SECRET_KEY);
-
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
     public String generateToken(User user) {
-
         return Jwts
                 .builder()
                 .subject(user.getEmail())
                 .issuedAt(new Date())
-                .expiration(
-                        new Date(
-                                System.currentTimeMillis()
-                                        + 1000 * 60 * 60
-                        )
-                )
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .signWith(getSignInKey())
                 .compact();
     }
 
     public String extractUsername(String token) {
-
         return Jwts
                 .parser()
                 .verifyWith((SecretKey) getSignInKey())
@@ -52,7 +47,6 @@ public class JwtService {
     }
 
     private Date extractExpiration(String token) {
-
         return Jwts
                 .parser()
                 .verifyWith((SecretKey) getSignInKey())
@@ -63,16 +57,12 @@ public class JwtService {
     }
 
     private boolean isTokenExpired(String token) {
-
-        return extractExpiration(token)
-                .before(new Date());
+        return extractExpiration(token).before(new Date());
     }
 
-    public boolean isTokenValid(String token, User user) {
-
+    // Alterado de User para UserDetails — menos acoplamento
+    public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
-
-        return username.equals(user.getEmail())
-                && !isTokenExpired(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
 }
