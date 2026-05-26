20def541e7ad13bf7141b0a41d4b3fea761a44b2
Bruno <brunoaespinace@hotmail.com>
2026-05-26 11:44:13 -0300

    Now User implements UserDetails for Authentication, login user now uses email, we have getAuthorities for future roles and new validations

--- src/main/java/com/bruno/banking_platform/auth/domain/User.java
+++ src/main/java/com/bruno/banking_platform/auth/domain/User.java
@@ -1,11 +1,13 @@
 package com.bruno.banking_platform.auth.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.UUID;
 
 @Entity
@@ -15,7 +17,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -29,4 +31,40 @@ public class User {
     private String password;
 
     private LocalDateTime createdAt;
+
+    // UserDetails — qual o "login" desse usuário
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    // UserDetails — permissões/roles (vazio por enquanto, implementamos depois)
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    // UserDetails — conta expirada?
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // UserDetails — conta bloqueada?
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // UserDetails — credenciais expiradas?
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // UserDetails — conta ativa?
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
