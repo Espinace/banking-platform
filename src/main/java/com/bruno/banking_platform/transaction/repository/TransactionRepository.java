4e6225d09c27d4a0a1a64fdb64219b1986b8a7d6
Bruno <brunoaespinace@hotmail.com>
2026-06-02 19:20:09 -0300

    Now we can seach on the database a list of transactions by senderAccountId and receiverAccountId

--- src/main/java/com/bruno/banking_platform/transaction/repository/TransactionRepository.java
+++ src/main/java/com/bruno/banking_platform/transaction/repository/TransactionRepository.java
@@ -3,8 +3,14 @@ package com.bruno.banking_platform.transaction.repository;
 import com.bruno.banking_platform.transaction.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository
         extends JpaRepository<Transaction, UUID> {
+
+    List<Transaction> findBySenderAccountIdOrReceiverAccountId(
+            UUID senderAccountId,
+            UUID receiverAccountId
+    );
 }
